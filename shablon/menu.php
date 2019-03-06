ede500ed0d3ced3e3c0e0961cb8d1ba5dd0f9f09
PulsarMira <pasha.tatarin57@yandex.ru>
2019-03-06 20:52:28 +0300

    Something

--- shablon/menu.php
+++ shablon/menu.php
@@ -1,7 +1,6 @@
 <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="index.php">Главная</a></li>
-        <li><a href="price.php">Услуги</a></li>
+        <li><a href="index.php">Главная</a></li>
         <li><a href="products.php">Товары</a></li>
         <li><a href="svyaz.php">Обратная связь</a></li>
         <li><a href="vhod.php">Вход и Регистрация</a></li>
